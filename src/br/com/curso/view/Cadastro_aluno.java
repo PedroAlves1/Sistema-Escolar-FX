fcf85986a134ae358de8c15d2cd36a6adfa94bca
Pedro <>
2013-10-15 13:25:20 -0300

    0.1

--- src/br/com/curso/view/Cadastro_aluno.java
+++ src/br/com/curso/view/Cadastro_aluno.java
@@ -4,10 +4,32 @@
  */
 package br.com.curso.view;
 
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Pedro
  */
-public class Cadastro_aluno {
-    
+public class Cadastro_aluno extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException  {
+         
+        Parent root = FXMLLoader.load(getClass().getResource("cadastro_aluno.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+
+    }
+        public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/br/com/curso/view/Login.java
+++ src/br/com/curso/view/Login.java
@@ -4,10 +4,29 @@
  */
 package br.com.curso.view;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
- * @author Pedro
+ * @author pedro
  */
-public class Login {
-    
+public class Login extends Application {
+        
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
